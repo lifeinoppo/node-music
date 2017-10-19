3d10ca936349d688118b471107506295c8c6c0a7
lifeinbofyx@sina.com <lifeinbofyx@sina.com>
2017-10-19 21:20:51 +0800

    normal update
    fix commor problem.

--- app.js
+++ app.js
@@ -194,7 +194,7 @@ function getRandomNum(Min, Max)
   var Range = Max - Min;   
   var Rand = Math.random();   
   return(Min + Math.round(Rand * Range));
-},
+}
 /*    
    record music dataUrl end  
 */
